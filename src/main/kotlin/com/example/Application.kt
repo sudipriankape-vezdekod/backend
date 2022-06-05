03532bf30f88ac19e6e87565608fb4a1c5164761
Nikitat0 <nikitatzero@yandex.ru>
2022-06-05 11:39:23 +0700

    task 30

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -13,6 +13,9 @@ import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.greaterEq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.lessEq
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
 
@@ -23,6 +26,7 @@ object Artists : IntIdTable() {
 object Votes : IntIdTable() {
     val phone = varchar("phone", 10)
     val artist = text("artist")
+    val timestamp = long("timestamp")
 }
 
 fun main() {
@@ -58,7 +62,7 @@ fun Application.configureRouting() {
             val (phone, artist) = call.receive<Vote>()
             if (!isPhoneValid(phone)) {
                 call.respond(HttpStatusCode.BadRequest)
-            } else if (transaction { Artists.select { Artists.name eq artist }.count() } != 0L) {
+            } else if (transaction { Artists.select { Artists.name eq artist }.count() } == 0L) {
                 call.respond(HttpStatusCode.NotFound)
                 return@post
             } else {
@@ -66,13 +70,43 @@ fun Application.configureRouting() {
                     Votes.insert {
                         it[Votes.phone] = phone
                         it[Votes.artist] = artist
+                        it[Votes.timestamp] = System.currentTimeMillis()
                     }
                 }
                 call.respond(HttpStatusCode.Created)
             }
         }
+        get("votes/stats") route@{
+            val from = call.request.queryParameters["from"]?.toLong()
+            val to = call.request.queryParameters["to"]?.toLong()
+            val intervalsN = call.request.queryParameters["intervals"]?.toLong()
+            val artists = call.request.queryParameters["artists"]?.split(',')
+            val stats =
+                transaction {
+                    val from =
+                        from ?: Votes.slice(Votes.timestamp.min()).selectAll().map { it[Votes.timestamp.min()] }[0]!!
+                    val to = to ?: Votes.slice(Votes.timestamp.max()).selectAll().map { it[Votes.timestamp.max()] }[0]!!
+                    val intervalsN = minOf(intervalsN ?: 10, (to - from) / 1000).toInt()
+                    val intervals = MutableList(intervalsN) { (to - from) / intervalsN }
+                    intervals[intervalsN - 1] += (to - from) % intervalsN
+                    intervals.add(0)
+                    val stats = mutableListOf<Stat>()
+                    var st = from
+                    var ed = from + intervals[0]
+                    var i = 1
+                    while (i <= intervalsN) {
+                        var condition = Votes.timestamp greaterEq st and (Votes.timestamp lessEq ed)
+                        artists?.let { condition = condition and (Votes.artist inList it) }
+                        stats.add(Stat(st, ed, Votes.select { condition }.count()))
+                        st = ed
+                        ed += intervals[i]
+                        i += 1
+                    }
+                    stats
+                }
+            call.respond(mapOf("data" to stats))
+        }
     }
-    routing {}
 }
 
 fun isPhoneValid(phone: String): Boolean {
@@ -87,3 +121,6 @@ data class Vote(val phone: String, val artist: String)
 
 @Serializable
 data class ArtistStat(val name: String, val votes: Long)
+
+@Serializable
+data class Stat(val start: Long, val end: Long, val votes: Long)
